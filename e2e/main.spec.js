11e570695f5518532b4b8fc1ebfe30eb1a40880b
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-07 13:48:32 +0200

    e2e tests

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -4,18 +4,18 @@ describe('The main view', function () {
   var page;
 
   beforeEach(function () {
-    browser.get('/index.html');
+    browser.get('/');
     page = require('./main.po');
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+    expect(page.h1El.getText()).toBe('Hello');
+    //expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
+    //expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
 
-  it('should list more than 5 awesome things', function () {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
+  it('should list nothing', function () {
+    expect(page.thumbnailEls.count()).toBe(0);
   });
 
 });
