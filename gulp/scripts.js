9f8e6afa1ae77da178bf3dcc737edb77d3017cd5
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-10 09:08:43 +0200

    typescript

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -5,16 +5,16 @@ var gulp = require('gulp');
 var conf = require('../gulpfile.config');
 var stylish = require('gulp-jscs-stylish');
 var browserSync = require('browser-sync');
-var noop = function () {};
 
 var $ = require('gulp-load-plugins')();
 
 var noop = function () {};
+//var mainFile = path.join(conf.paths.src, conf.paths.main, conf.paths.main + '.js');
 
-gulp.task('scripts', function () {
+gulp.task('scripts', ['typescripts'], function () {
   return gulp.src([
-    path.join(conf.paths.src, '/modules/**/*.js'),
-    path.join(conf.paths.src, '/main/app.js')
+    path.join(conf.paths.src, '/**/*.js'),
+    path.join('!' + conf.paths.src, '/librairies/**/*.js')
   	])
     .pipe($.jshint()) // jshint, it follows .jshintrc
     .pipe($.jscs()) // jscs, it follows .jscsrc
---
+++ sources/main/app.ts
@@ -0,0 +1,1 @@
+/// <reference path="../../.tmp/typings/tsd.d.ts" />
