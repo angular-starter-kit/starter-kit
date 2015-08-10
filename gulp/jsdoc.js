dfa9b6957117c4bb0abf737d361dda804dd33079
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-10 09:15:41 +0200

    jsdoc with angular template

---
+++ gulp/jsdoc.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var shell = require('gulp-shell');
+
+gulp.task('docs', shell.task([
+ 'node node_modules/jsdoc/jsdoc.js '+
+   '-c node_modules/angular-jsdoc/conf.json '+   // config file
+   '-t node_modules/angular-jsdoc/template '+    // template file
+   '-d report/docs '+                             // output directory
+   './README.md ' +                              // to include README.md as index contents
+   '-r sources/modules/'                              // source code directory
+]));
