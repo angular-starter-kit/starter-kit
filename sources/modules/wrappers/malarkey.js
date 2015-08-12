da948f14854cf2e819ef56090e41aa38af540273
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-12 14:19:25 +0200

    remove malarkey directive

--- sources/modules/wrappers/malarkey.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * Wraps malarkey.js into an angular module.
- */
-angular.module('malarkey', []).factory('malarkey', function ($window) {
-  // assumes malarkey has already been loaded on the page
-  return $window.malarkey;
-});
