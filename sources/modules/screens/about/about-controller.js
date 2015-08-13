4d5d9fca8ee0a292edb7e6f8f5ec039db561a2c8
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-13 18:27:29 +0200

    minor changes on about section

--- sources/modules/screens/about/about-controller.js
+++ sources/modules/screens/about/about-controller.js
@@ -2,9 +2,10 @@
 
 angular
   .module('about')
-  .controller('aboutController', function($scope, logger) {
+  .controller('aboutController', function($scope, logger, config) {
 
     logger = logger.getLogger('about');
     logger.info('about');
+    $scope.version = config.version;
 
   });
