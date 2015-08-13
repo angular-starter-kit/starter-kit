e35943eb46d40263aad31841355c6e4a22c9eaef
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-13 18:55:36 +0200

    changes on navbar

--- sources/modules/shell/shell.js
+++ sources/modules/shell/shell.js
@@ -5,11 +5,20 @@
  */
 
 angular
-	.module('shell', ['ui.router', 'logger'])
+	.module('shell', ['ui.router', 'logger', 'loading'])
 
-	.controller('shellController', function(logger) {
+	.controller('shellController', function(logger, $scope, $timeout, config, $locale, gettextCatalog) {
 
       	logger = logger.getLogger('shell');
       	logger.info('begin');
-
+      	$scope.currentLanguage = $locale.id;
+      	$scope.languages = config.supportedLanguages;
+      	$scope.$watch('currentLanguage', function(){
+      		gettextCatalog.setCurrentLanguage($scope.currentLanguage);
+      	});
+      	$scope.isLoading = true;
+      	$scope.message = 'wait';
+      	$timeout(function(){
+      		$scope.isLoading = false;
+      	}, 1000);
 	});
