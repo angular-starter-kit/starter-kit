396cff331c430e2d58a73ef5be64c83fdfb63999
Julien Marinescu <julien.marinescu.thales@gmail.com>
2015-08-14 16:29:38 +0200

    ngdoc + rm app-config

--- sources/modules/shell/shell-controller.js
+++ sources/modules/shell/shell-controller.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/**
+ * @ngdoc controller
+ * @name shellController
+ * @requires $scope
+ * @description
+ *  Controller supporting shared data.
+ * @property {String} currentLanguage current language.
+ * @property {Hash} languages collection of supported languages.
+ * @property {Boolean} isLoading used in `uiLoading` directive.
+ * @property {String} message  used in `uiLoading` directive.
+ */
+
 angular
 	.module('shell')
 	.controller('shellController', function(logger, 
--- sources/modules/ui-components/loading/loading-directive.js
+++ sources/modules/ui-components/loading/loading-directive.js
@@ -1,16 +1,16 @@
 'use strict';
 
 /**
- * Loading directive: displays a loading indicator while data is being loaded.
- *
- * Example usage: <div ui-loading="isLoading"></div>
- * The expected value of the directive attribute is a boolean indicating whether the content
- * is still loading or not.
- *
- * Additional parameter attributes:
- * - message: the loading message to display (none by default)
- *
- * Example: <div ui-loading="isLoading" message="Loading..."></div>
+ * @ngdoc directive
+ * @name uiLoading
+ * @description
+ *   Loading directive: displays a loading indicator while data is being loaded.
+ * @param {Boolean} ui-loading
+ *   When True show the content.
+ * @param {String} message
+ *   The loading message to display (none by default).
+ * @example
+ *   <div ui-loading="isLoading" message="Loading..."></div>
  */
 angular
   .module('loading', [])
