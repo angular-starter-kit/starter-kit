89d00dc58baf6b2e16a3a88f35ac55e7dffb764c
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-04-21 11:16:34 +0200

    Fixed merge

--- sources/main/main.constants.ts
+++ sources/main/main.constants.ts
@@ -1,46 +1,44 @@
 import app from 'main.module';
 
-  // Do not remove the comments below, or change the values. It's the markers used by gulp build task to change the
-  // value of the config constant when building the application, while removing the code below for all environments.
-  // replace:environment
-  let environment = {
-    local: {
-      debug: true,
+// Do not remove the comments below, or change the values. It's the markers used by gulp build task to change the
+// value of the config constant when building the application, while removing the code below for all environments.
+// replace:environment
+let environment = {
+  local: {
+    debug: true,
 
-      // REST backend configuration, used for all web services using restService
-      server: {
-        url: '',
-        route: 'api'
-      }
-    },
-    production: {
-      debug: false,
-      server: {
-        url: '',
-        route: 'api'
-      }
+    // REST backend configuration, used for all web services using restService
+    server: {
+      url: '',
+      route: 'api'
     }
-  };
+  },
+  production: {
+    debug: false,
+    server: {
+      url: '',
+      route: 'api'
+    }
+  }
+};
+// endreplace
+
+/**
+ * Defines app-level configuration.
+ */
+app.constant('config', {
+
+  // Do not remove the comments below, or change the values. It's the markers used by gulp build task to inject app
+  // version from package.json and environment values.
+  // replace:constant
+  version: 'dev',
+  environment: environment.local,
   // endreplace
 
-  /**
-   * Defines app-level configuration.
-   */
-  angular
-    .module('app')
-    .constant('config', {
-
-      // Do not remove the comments below, or change the values. It's the markers used by gulp build task to inject app
-      // version from package.json and environment values.
-      // replace:constant
-      version: 'dev',
-      environment: environment.local,
-      // endreplace
-
-      // Supported languages
-      supportedLanguages: [
-        'en-US',
-        'fr-FR'
-      ]
+  // Supported languages
+  supportedLanguages: [
+    'en-US',
+    'fr-FR'
+  ]
 
 });
